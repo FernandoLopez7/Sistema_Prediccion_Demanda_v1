1a5c10d9c5fd3fb06ac8b31366c5f55c90797907
Erik Murminacho <erikmurminacho07@outlook.com>
2026-04-22 15:28:10 -0500

    Fix dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
+import { ReactNode } from "react";
 
-export default function DashboardLayout({ children }) {
+export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Navbar />
