5ea82281d32d7a4d7f70df04413ecf29c64dd658
Erik Murminacho <erikmurminacho07@outlook.com>
2026-04-22 19:46:48 -0500

    Fix logo centrado

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,12 +19,12 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row md:justify-between md:items-center py-6 gap-4">
           <div className="flex items-center gap-4">
-            <div className="flex justify-center mb-4">
+            <div className="flex">
               <Image
                 src="/logo-bm-black.png"
                 alt="BiMetal Logo"
-                width={200}
-                height={70}
+                width={120}
+                height={50}
                 priority
               />
             </div>
